6047c945d71d38276f95ec03da320194a5544f04
Beatrice Bianca <bianca.luca@mindit.ro>
2018-10-22 20:30:50 +0300

    Changes

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
 from os import walk
 from collections import defaultdict
+import re
+
+
+def find__hole__word(w):
+    return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
 
 
 def print__dictionary__list(dictionary__list):
@@ -11,55 +16,55 @@ def print__dictionary__list(dictionary__list):
 def merge__dictionary(dict):
     # for k in sorted(dict, key=lambda k: len(dict[k])):
     #     print(k)
-    mergedlist = []
+
+    # mergedlist = []
     sorted_dictionary = sorted(dict, key=lambda k: len(dict[k]))
     i, j = 0, 0
+    k = 0
     while i < len(dict[word1]) and j < len(dict[word2]):
         if dict[word1][i] == dict[word2][j]:
-            mergedlist.append(dict[word1][i])
+            filePath = list(dictionary.keys())[list(dictionary.values()).index(dict[word1][i])]
+            directory = filePath[(filePath.rindex("/")-1)::]
+            print(directory)
+            k = k + 1
+            # mergedlist.append(dict[word1][i])
             i = i + 1
             j = j + 1
         elif dict[word1][i] < dict[word2][j]:
             i = i + 1
         else:
             j = j + 1
-    return mergedlist
+    # return mergedlist
+    # print(k)
 
 
 # os.chdir('D:/Facultate/Master/Regasirea Informatiei/Lab1/real_data/')
 os.chdir('C:/Users/Eva/Desktop/Information retrieval/Lab/Lab 1 - index, query and compression/real_data/0/')
+# root = 'D:/Facultate/Master/Regasirea Informatiei/Lab1/real_data/'
 root = 'C:/Users/Eva/Desktop/Information retrieval/Lab/Lab 1 - index, query and compression/real_data/'
 i = 0
 j = 0
-word1 = 'we'
-word2 = 'are'
+word1 = 'standford'
+word2 = 'class'
 dictionary = {}
 dictionaryList = defaultdict(list)
-# for dirpath, dirnames, filenames in walk('D:/Facultate/Master/Regasirea Informatiei/Lab1/real_data/'):
 for dirpath, dirnames, filenames in walk(root):
     for file in filenames:
         dictionary[dirpath + "/" + file] = i
         i = i + 1
-        if j <= 1:
-            try:
-                with open(dirpath + "/" + file, 'r') as f:
-                    fileContent = f.read()
-                    if word1 in fileContent:
-                        dictionaryList[word1].append(i)
-                    if word2 in fileContent:
-                        dictionaryList[word2].append(i)
-            except IOError as e:
-                print(e)
-    j = j + 1
+        try:
+            with open(dirpath + "/" + file, 'r') as f:
+                fileContent = f.read()
+                if find__hole__word(word1)(fileContent) is not None:
+                    dictionaryList[word1].append(i)
+                if find__hole__word(word2)(fileContent) is not None:
+                    dictionaryList[word2].append(i)
+        except IOError as e:
+            print(e)
 
-# Test: print first 10 files
-for item in dictionary:
-    if dictionary[item] <= 10:
-        print(item)
-
-print__dictionary__list(dictionaryList)
-list1 = dictionaryList.get(word1)
-list2 = dictionaryList[word2]
-print(len(list1))
-print(len(list2))
-print(merge__dictionary(dictionaryList))
+# print__dictionary__list(dictionaryList)
+# list1 = dictionaryList.get(word1)
+# list2 = dictionaryList[word2]
+# print(len(list1))
+# print(len(list2))
+merge__dictionary(dictionaryList)
